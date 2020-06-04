f4b9232c54dfc8a439a7396053664d2678ef2baa
ethull <ethan.hullett@btinternet.com>
2020-06-04 11:29:05 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 //import { Counter } from './features/counter/Counter';
+import Home from "./features/home/Home";
 import Hash from "./features/hash/Hash";
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="container">
-      {/* <Counter /> */}
-      <Hash />
-    </div>
+    <Router>
+      <div>
+        <Route exact path={"/"} component={Home} />
+        <Route path={"/hash"} component={Hash} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/features/home/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Navbar from "../../common/NavBar";
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <h1> landing page! </h1>
+      </div>
+    );
+  }
+}
